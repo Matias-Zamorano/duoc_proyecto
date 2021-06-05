6ea6d194f5a2fde73707c0b07835087466f2b748
matias zamorano <mat.zamoranoc@duocuc.cl>
2021-06-04 22:47:54 -0400

    se termino de realizar la tabla de manera correcta

--- js/Cursos.js
+++ js/Cursos.js
@@ -3,6 +3,7 @@ const cursos = [];
 const cargarTabla = ()=>{
     //1.Una referencia a la tabla
     let tbody  = document.querySelector("#tbody-curso");
+    tbody.innerHTML=""; // hace que no se multiplique los registros
     //2.Por cada registro generar una fila 
     for(let i=0; i <  cursos.length; ++ i){
         let c  = cursos[i];
@@ -20,7 +21,25 @@ const cargarTabla = ()=>{
 
         tdNombre.innerText = c.nombre;
         tdCorreo.innerText = c.correo;
+
+        
+        let icono = document.createElement("i");
+        
+        if(c.tipo == "1"){//<i class="far fa-file-code"></i>
+            icono.classList.add("far","fa-file-code","text-primary","fa-2x");
+        }else if(c.tipo =="2"){//<i class="far fa-comments"></i>
+            icono.classList.add("fas","fa-comments","text-primary","fa-2x");
+        }else if (c.tipo=="3"){//<i class="fas fa-language"></i>
+            icono.classList.add("fas","fa-language","text-primary","fa-x2");
+        }else if (c.tipo=="4"){//<i class="fas fa-pen"></i>
+            icono.classList.add("fas","fa-pen","text-primary","fa-2x");
+        }else{//<i class="fas fa-microphone"></i>
+            icono.classList.add("fas","fa-microphone","text-primary","fa-2x");
+        }
         tdTipo.innerText   = c.tipo;
+        tdTipo.appendChild(icono);
+        tdTipo.classList.add("text-center");
+        
         tdNro.innerText    = i + 1;
 
         tr.appendChild(tdNro);
@@ -47,5 +66,17 @@ document.querySelector("#cursos-form").addEventListener('submit', (e)=>{
     curso.tipo   = tipo;
     cursos.push(curso);
     cargarTabla();
+    
+    Swal.fire({
+        title: 'Curso registrado de manera correcta',
+        text:'Le llegara un mensaje que le indicara toda la informacion sobre el curso y sus horarios',
+        icon:'success',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
     
 });
